1196695601e76842ae85e63d49150a32d67ab2ef
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2025-04-21 01:00:45 -0300

    add fake data to carousel, style

--- src/components/projects/Carousel.tsx
+++ src/components/projects/Carousel.tsx
@@ -1,19 +1,41 @@
 import { useState, useEffect } from "react";
 import styles from "./Carousel.module.css";
+import { iconMap } from "../../utilities/icons";
 
 export default function Carousel() {
   const [current, setCurrent] = useState(0);
-  const [slidesPerView, setSlidesPerView] = useState(3);
-  const items = ["Blog", "Photo Tagging", "File Storage"];
+  const [slidesPerView, setSlidesPerView] = useState(2);
+
+  const items = [
+    {
+      title: "Blog",
+      description:
+        "A simple and clean blog platform for writing and sharing posts.",
+      bg: "https://lacvets.com/wp-content/uploads/2023/01/what-is-a-cats-lifespan-lakeland-fl-scaled.jpg",
+      url: "https://www.google.com",
+      stack: ["React", "CSS", "Express.js", "Node.js"],
+    },
+    {
+      title: "Photo Tagging",
+      description: 'A <em>"Where\'s Waldo"</em> &nbsp;style game.',
+      bg: "https://www.vets4pets.com/siteassets/species/cat/cat-close-up-of-side-profile.jpg",
+      stack: ["React", "CSS", "Express.js", "Node.js"],
+    },
+    {
+      title: "File Storage",
+      description:
+        "Secure cloud storage solution with easy access and sharing.",
+      bg: "https://cdn.britannica.com/39/226539-050-D21D7721/Portrait-of-a-cat-with-whiskers-visible.jpg",
+      stack: ["React", "CSS", "Express.js", "Node.js"],
+    },
+  ];
 
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth < 768) {
+      if (window.innerWidth < 1024) {
         setSlidesPerView(1);
-      } else if (window.innerWidth < 1024) {
-        setSlidesPerView(2);
       } else {
-        setSlidesPerView(3);
+        setSlidesPerView(2);
       }
     };
 
@@ -43,13 +65,31 @@ export default function Carousel() {
           }}
         >
           {items.map((item, index) => (
-            <div
+            <a
               key={index}
               className={styles.slide}
-              style={{ width: `${100 / slidesPerView}%` }}
+              style={{
+                width: `${100 / slidesPerView}%`,
+                backgroundImage: `url(${item.bg})`,
+              }}
+              href={item.url}
+              target="blank"
             >
-              <div className={styles.content}>{item}</div>
-            </div>
+              <div className={styles.content}>
+                <div className={styles.top}>
+                  <h4 className="title titleSmall">{item.title}</h4>
+                  <div className={styles.stack}>
+                    {item.stack?.map((icon, index) => (
+                      <i key={index} className={iconMap[icon]?.colored} />
+                    ))}
+                  </div>
+                </div>
+                <p
+                  className={styles.description}
+                  dangerouslySetInnerHTML={{ __html: item.description }}
+                ></p>
+              </div>
+            </a>
           ))}
         </div>
 
