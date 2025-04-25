f1b3597794dbb2e1ed71b144d82e6b1a7e2956cf
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2025-04-25 18:24:46 -0300

    position content cards on top of images

---
+++ src/utilities/projects.ts
@@ -0,0 +1,25 @@
+export const featuredProjects = [
+  {
+    title: "Blog",
+    description: "A simple and clean blog platform including auth system.",
+    bgDark: "/projects/blogDark.png",
+    bgLight: "/projects/blogLight.png",
+    url: "https://www.google.com",
+    stack: ["React", "CSS", "Express.js", "Node.js"],
+  },
+  {
+    title: "Photo Tagging",
+    description: 'A <em>"Where\'s Waldo"</em> &nbsp;style game.',
+    bgDark: "/projects/photoTagging.png",
+    bgLight: "/projects/photoTagging.png",
+
+    stack: ["React", "CSS", "Express.js", "Node.js"],
+  },
+  {
+    title: "File Storage",
+    description: "Secure cloud storage solution with easy access and sharing.",
+    bgDark: "/projects/fileStorage.png",
+    bgLight: "/projects/fileStorage.png",
+    stack: ["React", "CSS", "Express.js", "Node.js"],
+  },
+];
